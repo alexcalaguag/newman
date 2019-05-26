1f65d3d83736d5ed9ea52eac4b92cca9753a5209
Alex Manuel Calagua Gavilan <gavilan@lnxict019247.cit>
2019-05-26 19:54:47 -0300

    add cassandra e newman

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const util = require('./util/util');
+global.__basedir = __dirname;
+
+const util = require(__basedir + '/util/util');
 const param_env = process.argv[2];
-
-global.enum_env = util.string_of_enum(param_env);
 global.properties = util.getProperties();
+global.env_selected = util.string_of_enum(param_env);
 
-console.log('Execute Regression Test '+ enum_env);
+
+console.log('Execution of the regressive test '+properties.get('CONFIG.collection-name')+' in the '+ env_selected +' environment');
 
 var promiseStep1 = new Promise(function(resolve,reject){
 	util.cleanCassandra(resolve,reject);
 });
 
 var promiseStep2 = new Promise(function(resolve,reject){
+	console.log("Successful data cleansing");
 	util.executeNewman(resolve,reject);
 })
 
 promiseStep1.then(value => promiseStep2)
-			.then(value => {
-				console.log(value);	
-});
+						.then(value => {console.log(value);});
--- util/util.js
+++ util/util.js
@@ -1,74 +1,66 @@
 
 exports.string_of_enum = function(value) {
-	const env = {
+	const envs = {
 		LOCAL:'LOCAL',
 		DEV:'DEV',
 		HOMOLOG:'HOMOLOG'
 	}
-	for (var k in env){ 
-		if (env[k]==value){
-		 	return k;
+	for (var env in envs){
+		if (envs[env] == value){
+		 	return env;
 		}
 	}
-	throw new Error('Enviroment '+value+' is not defined ');		
+	throw new Error('Environment '+ value +' is not defined ');
 }
 
 exports.getProperties = function() {
 	var PropertiesReader = require('properties-reader');
-	return PropertiesReader('./config/config.properties');	
+	return PropertiesReader(__basedir +'/config/config.properties');
 }
 
 exports.cleanCassandra = function(resolve, reject) {
-
-	console.log(properties.get(enum_env+'.url'));
+	console.log(properties.get(env_selected+'.url'));
 	var querys = [];
 	properties.each((key, value) => {
 		var name = key.substring(0, key.indexOf("."));
 		if(name == 'query'){
-			var obj1 = { query: value};
-			querys.push(obj1);
+			var row = { query: value};
+			querys.push(row);
 		}
-	})
-	console.log(querys);
+	});
+	var cassandra = require('cassandra-driver');
+	var client = new cassandra.Client({contactPoints:[properties.get(env_selected+'.url')],
+																		 localDataCenter:'datacenter1',
+																		 keysapce:properties.get(env_selected+'.keyspace')});
 
-	/*var cassandra = require('cassandra-driver');
-	var client = new cassandra.Client({contactPoints:[properties.get(enum_env+'.url')],localDataCenter:'datacenter1',keysapce:''});
-
-	var query = properties.get('query.query1');
-	client.execute(query,function(err,result){
-		resolve("Limpeza de dados realizada com sucesso");
-
-	});*/
-
-	resolve("Limpeza de dados realizada com sucesso");
+	client.batch(querys, { prepare: true },function(err,result){
+		client.shutdown();
+		resolve("Shutdown Cassandra");
+	});
 }
 
 exports.executeNewman = function(resolve, reject) {
-
-	/*
-	var newman=require('newman');
-
+	var newman = require('newman');
 	newman.run({
-		collection:require('./collection/API-ADM.postman_collection.json'),
-		environment:require('./collection/'+enum_env+'.postman_enviroment.json'),
+		collection: require(__basedir +'/'+ properties.get('FOLDER.collection') +'/'+ properties.get('CONFIG.collection-name') +'.postman_collection.json'),
+		environment: require(__basedir +'/'+ properties.get('FOLDER.collection') +'/'+env_selected+'.postman_environment.json'),
 		reporters: ['html','cli','htmlextra'],
-		reporter:{
-			html:{export :'./report/htmlReport.html'},
-			htmlextra:{export:'.report/htmlReportExtra.html'}
+		reporter: {
+			html: { export : __basedir +'/'+ properties.get('FOLDER.report') +'/'+ properties.get('CONFIG.collection-name') +'HtmlReport.html'},
+			htmlextra: {
+				export: __basedir +'/'+ properties.get('FOLDER.report') +'/'+ properties.get('CONFIG.collection-name') +'HtmlReportExtra.html'
+			}
 		},
 		insecure:true,
 		timeout:180000
 	}).on('start',function(err,args){
-		console.log('running a collection...');
+		console.log('Running collection '+ properties.get('CONFIG.collection-name') +'.postman_collection.json');
 	}).on('done',function(err,summary){
-		if(err,summary.error){
-			console.erro('collection run encountered and error.');
-
+		if(err || summary.error){
+			console.erro('Collection run encountered and error.');
 		}else {
-			console.log('collection run completed.');
-			resolve('relatorio gerado');
+			console.log('Collection '+properties.get('CONFIG.collection-name')+'.postman_collection.json successfully executed.');
+			resolve('Report '+ properties.get('CONFIG.collection-name') +'HtmlReportExtra.html generated successfully in the folder '+ properties.get('FOLDER.report'));
 		}
-	});*/
-				resolve('relatorio gerado');
+	});
 }
-
